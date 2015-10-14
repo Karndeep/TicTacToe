2552e68fc6df4d61c971fd50894d8aa10017ef98
Karndeep <kkahlon3672@conestogac.on.ca>
2015-10-14 17:05:49 -0400

    First Commit

---
+++ TicTacToeKKAssigment/TicTacToeKKAssigment/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace TicTacToeKKAssigment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.A3 = new System.Windows.Forms.PictureBox();
+            this.B1 = new System.Windows.Forms.PictureBox();
+            this.B3 = new System.Windows.Forms.PictureBox();
+            this.C1 = new System.Windows.Forms.PictureBox();
+            this.C3 = new System.Windows.Forms.PictureBox();
+            this.C2 = new System.Windows.Forms.PictureBox();
+            this.A2 = new System.Windows.Forms.PictureBox();
+            this.B2 = new System.Windows.Forms.PictureBox();
+            this.A1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.A3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.B1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.B3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.C1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.C3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.C2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.A2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.B2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.A1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // A3
+            // 
+            this.A3.Location = new System.Drawing.Point(414, 41);
+            this.A3.Name = "A3";
+            this.A3.Size = new System.Drawing.Size(158, 87);
+            this.A3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.A3.TabIndex = 51;
+            this.A3.TabStop = false;
+            this.A3.Click += new System.EventHandler(this.picturebox_click);
+            // 
+            // B1
+            // 
+            this.B1.Location = new System.Drawing.Point(12, 187);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(158, 87);
+            this.B1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.B1.TabIndex = 50;
+            this.B1.TabStop = false;
+            this.B1.Click += new System.EventHandler(this.picturebox_click);
+            // 
+            // B3
+            // 
+            this.B3.Location = new System.Drawing.Point(414, 187);
+            this.B3.Name = "B3";
+            this.B3.Size = new System.Drawing.Size(158, 87);
+            this.B3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.B3.TabIndex = 49;
+            this.B3.TabStop = false;
+            this.B3.Click += new System.EventHandler(this.picturebox_click);
+            // 
+            // C1
+            // 
+            this.C1.Location = new System.Drawing.Point(12, 332);
+            this.C1.Name = "C1";
+            this.C1.Size = new System.Drawing.Size(158, 87);
+            this.C1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.C1.TabIndex = 48;
+            this.C1.TabStop = false;
+            this.C1.Click += new System.EventHandler(this.picturebox_click);
+            // 
+            // C3
+            // 
+            this.C3.Location = new System.Drawing.Point(414, 332);
+            this.C3.Name = "C3";
+            this.C3.Size = new System.Drawing.Size(158, 87);
+            this.C3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.C3.TabIndex = 47;
+            this.C3.TabStop = false;
+            this.C3.Click += new System.EventHandler(this.picturebox_click);
+            // 
+            // C2
+            // 
+            this.C2.Location = new System.Drawing.Point(214, 332);
+            this.C2.Name = "C2";
+            this.C2.Size = new System.Drawing.Size(158, 87);
+            this.C2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.C2.TabIndex = 46;
+            this.C2.TabStop = false;
+            this.C2.Click += new System.EventHandler(this.picturebox_click);
+            // 
+            // A2
+            // 
+            this.A2.Location = new System.Drawing.Point(214, 41);
+            this.A2.Name = "A2";
+            this.A2.Size = new System.Drawing.Size(158, 87);
+            this.A2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.A2.TabIndex = 44;
+            this.A2.TabStop = false;
+            this.A2.Click += new System.EventHandler(this.picturebox_click);
+            // 
+            // B2
+            // 
+            this.B2.Location = new System.Drawing.Point(214, 187);
+            this.B2.Name = "B2";
+            this.B2.Size = new System.Drawing.Size(158, 87);
+            this.B2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.B2.TabIndex = 45;
+            this.B2.TabStop = false;
+            this.B2.Click += new System.EventHandler(this.picturebox_click);
+            // 
+            // A1
+            // 
+            this.A1.Location = new System.Drawing.Point(12, 41);
+            this.A1.Name = "A1";
+            this.A1.Size = new System.Drawing.Size(158, 87);
+            this.A1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.A1.TabIndex = 52;
+            this.A1.TabStop = false;
+            this.A1.Click += new System.EventHandler(this.picturebox_click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::TicTacToeKKAssigment.Properties.Resources.Background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(584, 462);
+            this.Controls.Add(this.A3);
+            this.Controls.Add(this.B1);
+            this.Controls.Add(this.B3);
+            this.Controls.Add(this.C1);
+            this.Controls.Add(this.C3);
+            this.Controls.Add(this.C2);
+            this.Controls.Add(this.A2);
+            this.Controls.Add(this.B2);
+            this.Controls.Add(this.A1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.A3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.B1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.B3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.C1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.C3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.C2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.A2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.B2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.A1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox A3;
+        private System.Windows.Forms.PictureBox B1;
+        private System.Windows.Forms.PictureBox B3;
+        private System.Windows.Forms.PictureBox C1;
+        private System.Windows.Forms.PictureBox C3;
+        private System.Windows.Forms.PictureBox C2;
+        private System.Windows.Forms.PictureBox A2;
+        private System.Windows.Forms.PictureBox B2;
+        private System.Windows.Forms.PictureBox A1;
+    }
+}
+
---
+++ TicTacToeKKAssigment/TicTacToeKKAssigment/Form1.cs
@@ -0,0 +1,205 @@
+﻿/*
+ * Program Name: Assigment 2 Tic Tac Toe Game
+ *
+ * Purpose:This game is for 2 players and you play tic tac toe.
+ * The first user is X and the second user is O.
+ * Once the game is complete the user will be prompt to start a
+ * new game.
+ *
+ * Created: Karndeep Kahlon Oct 2015
+ * 
+ * Revision history:
+ * Karndeep Kahlon Oct 2015
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToeKKAssigment
+{
+    public partial class Form1 : Form
+    {
+        //Declaration & Initialize
+        int playerX = 1;
+        int playerO = 2;
+        int player;
+        string endGameMessage  = "Tie Game!";
+        int[] gridArray = new int[9];
+        int[,] winningCombo = new int[,]{
+                           {0,1,2},{3,4,5},{6,7,8},
+                           {0,3,6},{1,4,7},{2,5,8},
+                           {0,4,8},{2,4,6}};
+        public Form1()
+        {
+            InitializeComponent();
+            //Setting first Move to player X
+            player = playerX;
+
+        }
+
+        private void picturebox_click(object sender, EventArgs e)
+        {
+            //PictureClick is the declared as the picture that was clicked
+            PictureBox PictureClick = (PictureBox)sender;
+            //Checks if image is blank if yes then error message(For double click)
+            if (PictureClick.Image == null)
+            {
+                //Sets the image to x or o depending on the player value
+                if (player == playerX)
+                {
+                    PictureClick.Image = Properties.Resources.x;
+                    player = playerO;
+                }
+                else
+                {
+                    PictureClick.Image = Properties.Resources.o;
+                    player = playerX;
+                }
+                //Calls methods
+                insertIntoArray(getControlByName(PictureClick));
+                gameResult();
+
+            }
+            else
+            {
+                MessageBox.Show("This box has already been clicked!", "Error!");
+            }
+        }
+        //This method gets the control for picture by name
+        private string getControlByName(Control ctrl)
+        {
+            return ctrl.Name;
+        }
+        //This method is to add a value to the 
+        //array which is either 1 or 2 depending on player.
+        private void insertIntoArray(string name)
+        {
+            //Use the name of my pictureBox which I named A1,A2,A3 etc
+            switch (name)
+            {
+                case "A1":
+                    gridArray[0] = player;
+                    break;
+                case "A2":
+                    gridArray[1] = player;
+                    break;
+                case "A3":
+                    gridArray[2] = player;
+                    break;
+                case "B1":
+                    gridArray[3] = player;
+                    break;
+                case "B2":
+                    gridArray[4] = player;
+                    break;
+                case "B3":
+                    gridArray[5] = player;
+                    break;
+                case "C1":
+                    gridArray[6] = player;
+                    break;
+                case "C2":
+                    gridArray[7] = player;
+                    break;
+                case "C3":
+                    gridArray[8] = player;
+                    break;
+            }
+        }
+        //This method is to find the game result if the user has won
+        private void gameResult()
+        {   //Loops 9 times for each row 
+            for (int i = 0; i < winningCombo.GetLength(0); i++)
+            {   //Counter to check if they won.
+                int WinnerInput = 0;
+                //Loops 3 times for each column
+                for (int k = 0; k < winningCombo.GetLength(1); k++)
+                {
+                    //Game spot is the spot of the winning array
+                   int gameSpot = winningCombo[i, k];
+                    //Checks if grid array is equal to player value
+                    if (gridArray[gameSpot] == player)
+                    {
+                        WinnerInput++;
+                        //If counter hit 3 then user has won.
+                        if (WinnerInput == 3)
+                        {   //Determine which player has won
+                            if(player == playerO)
+                            {
+                                endGameMessage = "Player X has won";
+                            }
+                            else if(player == playerX)
+                            {
+                                endGameMessage = "Player O has won";
+                            }
+                            //Calling method with string endGameMessage
+                            showConfirmDialog(endGameMessage);
+                        }                        
+                    }
+                }
+            }
+            //If no one has won then checks for tie
+            tieCheck();
+        }
+        //Method for checking if tie
+        private void tieCheck()
+        {
+            //Declaration & Initialize
+            int count = 0;
+                //For is going through each gridArray value but only 
+                //adds to counter if gridArray has value.
+                for (int i = 0; i < gridArray.Length; i++)
+                {
+                     if (gridArray[i] !=0)
+                     {
+                         count++;
+                     }
+                }  
+            //If count is 9 then Game is tied.
+            if (count >= 9)
+            {
+                showConfirmDialog(endGameMessage);
+            }
+        }
+        //Method for Prompting the user
+        private void showConfirmDialog(string results)
+        {
+            DialogResult result;
+
+            result = MessageBox.Show(results, "Would you like to play again?", MessageBoxButtons.YesNo);
+
+            if (result == DialogResult.Yes)
+            {   //Calls method
+                resetBoard();
+            }
+            else
+            {
+                Application.Exit();
+            }
+        }
+        //Method that reset the player,endGameMessage array and picture image.
+        private void resetBoard()
+        {
+            //Resets all images for PictureBoxes
+            foreach (PictureBox pic in this.Controls)
+            {
+                pic.Image = null;
+            }
+            player = playerX;
+
+            endGameMessage = "Game is Tied";
+
+            //Reset Array and sets default values to 0;
+            for (int i = 0; i < gridArray.Length; i++)
+            {
+                     gridArray[i] = 0;
+            }
+        }
+    }
+}
